49667be39bc09250cd1ea2e6f47fc63c3615b9b0
Adela Máčajová <adela.macajova3@gmail.com>
2026-01-15 21:22:32 +0100

    vzhľad domovskej stránky

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="home-content">
-      <h1>Vitajte v Alvin's Pet Shop!</h1>
+      <div class="home-product-card">
+        <h2>Naše najlepšie produkty</h2>
+        <RouterLink to="/eshop/psy">Nakupovať</RouterLink>
+      </div>
+      <br>
+      <div class="home-blog-card">
+        <h2>Novinky zo sveta domácich miláčikov</h2>
+        <RouterLink to="/blog">Čítať blog</RouterLink>
+      </div>
   </div>
 </template>
 
 <script>
-
+import {RouterLink} from "vue-router"
 export default{
   components:{
+    RouterLink
     
   },
   data(){
